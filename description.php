5cd659851e8f13da8a553bc9e4cd26d0589f701a
Jovem-Blood <thiagopeixot13.gmail.com>
2020-06-01 17:20:22 -0300

    description final ;-;

--- description.php
+++ description.php
@@ -20,7 +20,7 @@
          padding: 5px;
          width: 400px;
          height: fit-content;
-         margin-left: 40px;
+         ;
        }
      </style>
 </head>
@@ -41,19 +41,21 @@
 
 <div class="content">
 
-<h1 class="text-center text-uppercase" style='font-weight: 700;'><?php echo str_replace('-',' ',$_GET['manga']); ?></h1>
+<h1 class="text-center text-uppercase" style='font-weight: 700;margin-top:2%;'><?php echo str_replace('-',' ',$_GET['manga']); ?></h1>
 
-<div class="">
 
-  <div class="desc w-25 p-3 ">
+<div class="" style="margin: 1%;">
+  <div class="desc" style="">
     <?php echo get_things($manga);?>
   </div>
-
+    <iframe class="float-right" width="350" height="500" src="https://discordapp.com/widget?id=711935936187072542&theme=dark"  allowtransparency="true" frameborder="0">
+    </iframe><br>
+    <h2 class="w-25 p-3"  style='font-weight: 700;margin-left: 40px;'>Capítulos#</h2>
 </div>
 
-<h2 class="w-25 p-3"  style='font-weight: 700;margin-left: 40px;'>Capítulos#</h2>
 
-<div class=" desc w-25 p-3 ">
+
+<div class=" desc w-25 p-3" style="margin-left:1%;">
 
 <?php
 $cap_max = $_GET['manga'];
@@ -61,7 +63,7 @@ $manga = $_GET['manga'];
 $cap_max = get_cap($manga);
 
 for($c =1; $c<=$cap_max; $c++){
-  echo "<a href='reader.php?cap=$c&manga=$manga' style='color:#3170c4;font-weight: 700;'>Capítulo #$c</a> <br>";
+  echo "<a  href='reader.php?cap=$c&manga=$manga' style='color:#3170c4;font-weight: 700;'>Capítulo #$c</a> <br>";
 }
 
  ?>
